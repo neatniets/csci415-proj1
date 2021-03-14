26f65702645c1c15e14c5cf5f09122f7fbfdf829
niets <mymailboxrebelled@gmail.com>
2021-03-14 17:36:25 -0500

    Finish keygen main program
    The printing of internal values has also been commented out. The key
    pairs are now stored in their respective files.

--- keygen.cpp
+++ keygen.cpp
@@ -5,6 +5,16 @@
 #include "rsa-exponents.hpp"
 #include "rsa-primes.hpp"
 
+/** Write the RSA key pair to a file.
+ * @param[in]   fp      file pointer to write to
+ * @param[in]   key_exp key exponent
+ * @param[in]   key_mod key modulus
+ * @pre         fp is a valid & open file pointer
+ * @pre         key_exp and key_mod are a valid key pair */
+static void
+write_key(FILE *fp,
+          const mpz_t key_exp,
+          const mpz_t key_mod);
 /** Print a line of help about program usage.
  * @param[in]   argc    argc from main
  * @param[in]   argv    argv from main */
@@ -51,34 +61,35 @@ int main(int argc, char **argv) {
         mpz_t q;
         mpz_init(q);
         find_primes(p, q, num_digits);
-        /* TODO: remove printing after testing */
-        fputs("p = ", stdout);
+        /*fputs("p = ", stdout);
         mpz_out_str(stdout, 10, p);
         fputc('\n', stdout);
         fputs("q = ", stdout);
         mpz_out_str(stdout, 10, q);
-        fputc('\n', stdout);
+        fputc('\n', stdout);*/
 
         /* get exponents */
         mpz_t e;
         mpz_t d;
         find_rsa_exp(p, q, e, d);
-        /* TODO: remove printing after testing */
-        fputs("e = ", stdout);
+        /*fputs("e = ", stdout);
         mpz_out_str(stdout, 10, e);
         fputc('\n', stdout);
         fputs("d = ", stdout);
         mpz_out_str(stdout, 10, d);
-        fputc('\n', stdout);
+        fputc('\n', stdout);*/
 
         /* get modulus */
         mpz_t n;
         mpz_init(n);
         mpz_mul(n, p, q); // n = p * q
-        /* TODO: remove printing after testing */
-        fputs("n = ", stdout);
+        /*fputs("n = ", stdout);
         mpz_out_str(stdout, 10, n);
-        fputc('\n', stdout);
+        fputc('\n', stdout);*/
+
+        /* print keys to files */
+        write_key(priv_fp, d, n);
+        write_key(pub_fp, e, n);
 
         mpz_clear(p);
         mpz_clear(q);
@@ -88,6 +99,16 @@ int main(int argc, char **argv) {
         fclose(priv_fp);
         fclose(pub_fp);
         return 0;
+}
+
+static void
+write_key(FILE *fp,
+          const mpz_t key_exp,
+          const mpz_t key_mod) {
+        mpz_out_str(fp, 10, key_exp); // write exponent
+        fputc('\n', fp); // newline separator
+        mpz_out_str(fp, 10, key_mod); // write modulus
+        fputc('\n', fp); // eof newline
 }
 
 static void
